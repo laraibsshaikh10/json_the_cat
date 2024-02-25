3c3f39285b91bae7cb21e20371b782a46235a320
laraibsshaikh10 <laraibsshaikh10@gmail.com>
2024-02-25 06:57:16 -0500

    Completed with all edge cases

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,14 +1,25 @@
 //import request library
 const request = require("request");
 
+//Allow the user to specify the breed name using command-line arguments.
+const specifyBreedName = process.argv.slice(2);
+
 //Enter the API endpoint/URL into the browser, searching for the breed Siberian.
-const apiUrl = "https://api.thecatapi.com/v1/breeds/search?q=siberian";
+//q=${specifyBreedName} takes in the query of breedname specified in the node by the user
+const apiUrl = `https://api.thecatapi.com/v1/breeds/search?q=${specifyBreedName}`;
+
+//if the specified breed name is not present, log it and exit
+if (!specifyBreedName) {
+  console.log("Invalid breed name.");
+  process.exit(1);
+}
 
 //Write the logic in breedFetcher.js to fetch the Siberian data from the API endpoint using request.
-const breedData = function () {
+const breedData = function(specifyBreedName) {
+ 
   request(apiUrl, (error, response, body) => {
     if (error) {
-      console.log("Error: ", error);
+      console.log("Error Occurred: ", error);
       //if status code is any other than 200
     } else if (response.statusCode !== 200) {
       console.log("Status Code Error: ", response.statusCode);
@@ -23,15 +34,15 @@ const breedData = function () {
         if (data.length > 0) {
           console.log("First entry description: ", data[0].description);
         } else {
-          console.log("Breed data not found.")
+          console.log("Breed name not found.");
         }
 
       //if there's an error while parsing json string, log it to the console
       } catch (parseErr) {
-        console.log("Error while parsing JSON body: ", parseErr)
+        console.log("Error while parsing JSON body: ", parseErr);
       }
     }
   });
 };
 
-breedData();
+breedData(specifyBreedName);
